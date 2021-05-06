a9a2bb72d2790e7e4240a27e28ace6d4ea8dc613
Anton Ekblad <anton@ekblad.cc>
2021-05-06 21:41:39 +0200

    Fix webpage title.

--- build-site.sh
+++ build-site.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-./mat.py -mv | pandoc -f markdown -t html -H templates/head.html -A templates/suffix.html -B templates/prefix.html > menu.html
+./mat.py -mv | \
+    pandoc -f markdown -t html \
+        -V pagetitle:"Lunchdags!" \
+        -H templates/head.html \
+        -A templates/suffix.html \
+        -B templates/prefix.html \
+        > menu.html
