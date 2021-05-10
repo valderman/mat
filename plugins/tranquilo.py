7500ff27e4242a73e9e537cedca8463a85b7fa48
Jonathan Skårstedt <jonathan.skarstedt@barium.se>
2021-05-10 11:19:25 +0200

    added a tranquilo plugin

---
+++ plugins/tranquilo.py
@@ -0,0 +1,23 @@
+import re
+
+def food(api, date):
+    if not api.is_current_week(date):
+        return []
+    if not api.is_today(date):
+        raise NotImplementedError("Tranquilo currently only supports menus for today")
+    if not api.soup:
+        return []
+
+    response = api.requests.get('https://tranquilo.se')
+    soup = api.soup(response.content, 'html.parser')
+    today = soup.find(id = "todays-lunch").select(
+        'li:has(> h1:-soup-contains(Dagens))')[0]
+    types = today.find_all("h3")
+    descriptions = today.find_all("p")[1:]
+    for (food_type, description) in zip(types, descriptions):
+        yield api.food(
+            food_type.get_text().strip(), 
+            description.get_text().strip())
+
+def name():
+    return "Tranquilo"
