86376749b9f443802211058838ef503c6584f7dd
Anton Ekblad <anton@ekblad.cc>
2021-12-08 09:21:55 +0100

    Fix Bee plugin.

--- plugins/bee.py
+++ plugins/bee.py
@@ -1,4 +1,5 @@
 import tempfile
+import re
 
 __days = [
     u"MÅNDAG",
@@ -30,16 +31,24 @@ def food(api, date):
     dagens_end_line = pdf_lines.index(__days[date.isoweekday()])
     dagens_lines = pdf_lines[dagens_line+2:dagens_end_line]
 
-    fish_line = pdf_lines.index(u"VECKANS FÅNGST (FISH OF THE")
+    fish_line = pdf_lines.index(u"VECKANS FISK (FISH OF THE WEEK)")
     veg_line = pdf_lines.index(u"VECKANS VEGETARISKA")
     veg_end_line = pdf_lines.index(u"ALLTID PÅ LUNCHEN")
 
     fish_lines = pdf_lines[fish_line+3:veg_line]
     veg_lines = pdf_lines[veg_line+3:veg_end_line]
 
+    def get_fish():
+        today_fish_text = u"MÅNDAG TILL ONSDAG" if date.isoweekday() < 4 else u"TORSDAG OCH FREDAG"
+        today_fish_line = pdf_lines.index(today_fish_text)
+        end_fish_line = pdf_lines.index(u"TORSDAG OCH FREDAG") if date.isoweekday() < 4 else veg_line
+        fish_lines = pdf_lines[today_fish_line+1:end_fish_line]
+        fish_heading = re.sub("\\(([^)]+)\\).*", "\\1", fish_lines[0])
+        return api.food(fish_heading, ' '.join(fish_lines))
+
     dagens = api.food(dagens_lines[0], ' '.join(dagens_lines[1:]))
-    fish = api.food(fish_lines[0], ' '.join(fish_lines[1:]))
-    vegetarian = api.food(veg_lines[0], ' '.join(veg_lines[1:]))
+    fish = get_fish()
+    vegetarian = api.food(None, ' '.join(veg_lines))
     return [dagens, fish, vegetarian]
 
 def name():
